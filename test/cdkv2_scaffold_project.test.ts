5b9f346409ad3cb7505f9acbae6d0cf0ee0e61c6
Alejandro Velez <wrvo@gft.com>
2026-04-28 12:57:18 -0500

    Initial commit

---
+++ test/cdkv2_scaffold_project.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Cdkv2ScaffoldProject from '../lib/cdkv2_scaffold_project-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdkv2_scaffold_project-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Cdkv2ScaffoldProject.Cdkv2ScaffoldProjectStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
